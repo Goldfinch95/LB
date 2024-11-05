b27539680d9cb6b8380c3199f03acab2b198d00d
Goldfinch <facu_vila@hotmail.com>
2024-11-04 23:55:38 -0300

    sixth comment.working on inicio component.adding second carrousel

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CarouselCardComponent } from '../../components/carousel-card/carousel-card.component';
+import * as brandsData from '../../../assets/brands.json';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,CarouselCardComponent,FooterComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
