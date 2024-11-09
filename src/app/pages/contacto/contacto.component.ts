9bb7b28f5e92fefc0a20ae1282b617a43ea97945
Goldfinch <facu_vila@hotmail.com>
2024-11-09 16:46:54 -0300

    se trabajo la pagina contactos.se agrego la extension de google maps y se modificaron las fuentes de las letras.

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,GoogleMapsModule, NgFor, FooterComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
-
+  center: google.maps.LatLngLiteral = { lat: -34.5846807869225, lng: -58.43609286260402 };
+  zoom = 16;
+  markerPositions: google.maps.LatLngLiteral[] = [
+    { lat: -34.5846807869225, lng: -58.43609286260402 }
+  ];
 }
