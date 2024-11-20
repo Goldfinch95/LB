cac9c296c13c17e9f93a8ec276787821947d8dda
Goldfinch <facu_vila@hotmail.com>
2024-11-20 19:43:21 -0300

    diseño responsivo del componente navbar y la pagina inicio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,13 +1,14 @@
 import { CarrouselComponent } from '../../components/carrousel';
 import { Component } from '@angular/core';
 import { MainContainerComponent } from '../../components/layout';
+import { SecondCarrouselComponent } from '../../components/second-carrousel';
 
 
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MainContainerComponent, CarrouselComponent],
+  imports: [CarrouselComponent, MainContainerComponent, SecondCarrouselComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
