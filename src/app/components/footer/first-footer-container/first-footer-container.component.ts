1205657356e890b71df941463e85a78ecce71ae3
Goldfinch <facu_vila@hotmail.com>
2024-12-17 10:15:08 -0300

    se agrego 2 botones en el footer

--- src/app/components/footer/first-footer-container/first-footer-container.component.ts
+++ src/app/components/footer/first-footer-container/first-footer-container.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-first-footer-container',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './first-footer-container.component.html',
   styleUrl: './first-footer-container.component.css'
 })
 export class FirstFooterContainerComponent {
-
+  faWhatsapp = faWhatsapp;
+  faEnvelope = faEnvelope;
 }
